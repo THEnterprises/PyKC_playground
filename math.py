bd19dd553cdb8bf43a46b9e61760f1cfc9da6157
VerusLogic <mike@veruslogic.com>
2015-01-10 12:12:25 -0600

    Adding multiply

--- math.py
+++ math.py
@@ -6,6 +6,10 @@ def add(x,y):
 def sub(x,y):
     return x - y
 
+def mult(x,y):
+    return x * y
+
 if '__main__' == __name__:
     print("3 + 4 should give....%d" % add(3,4))
     print("9 - 4 should give....%d" % sub(9,4))
+    print("9 * 4 should give....%d" % mult(9,4))
