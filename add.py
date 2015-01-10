d28071d65431c874d64e463d61a002d8b81c538a
VerusLogic <mike@veruslogic.com>
2015-01-10 12:00:51 -0600

    Added subtract func

--- add.py
+++ add.py
@@ -2,6 +2,10 @@ __author__ = 'Tracy'
 
 def add(x,y):
     return x + y
+	
+def sub(x,y):
+    return x - y
 
 if '__main__' == __name__:
     print("3 + 4 should give....%d" % add(3,4))
+    print("9 - 4 should give....%d" % sub(9,4))
